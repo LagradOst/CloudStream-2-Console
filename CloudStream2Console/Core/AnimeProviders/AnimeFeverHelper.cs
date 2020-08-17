fa7af6022fc4a577ff45fa15f244ca67f9bb27e7
LagradOst <balt.758@gmail.com>
2020-08-17 22:04:23 +0200

    ok, this should work

---
+++ CloudStream2Console/Core/AnimeProviders/AnimeFeverHelper.cs
@@ -0,0 +1,203 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CloudStreamForms.Core.AnimeProviders
+{
+    public class AnimeFeverHelper
+    {
+        public class AnimeFeverSearchPoster
+        {
+            public string source { get; set; }
+            public int width { get; set; }
+            public int height { get; set; }
+        }
+
+        public class AnimeFeverSearchPoster2
+        {
+            public int id { get; set; }
+            public string disk_name { get; set; }
+            public int file_size { get; set; }
+            public string content_type { get; set; }
+            public object title { get; set; }
+            public object description { get; set; }
+            public string field { get; set; }
+            public int sort_order { get; set; }
+            public string created_at { get; set; }
+            public string updated_at { get; set; }
+            public string path { get; set; }
+            public string extension { get; set; }
+        }
+
+        public class AnimeFeverSearchLogo
+        {
+            public int id { get; set; }
+            public string disk_name { get; set; }
+            public int file_size { get; set; }
+            public string content_type { get; set; }
+            public object title { get; set; }
+            public object description { get; set; }
+            public string field { get; set; }
+            public int sort_order { get; set; }
+            public string created_at { get; set; }
+            public string updated_at { get; set; }
+            public string path { get; set; }
+            public string extension { get; set; }
+        }
+
+        public class AnimeFeverSearchDatum
+        {
+            public int id { get; set; }
+            public string uid { get; set; }
+            public object anilist_id { get; set; }
+            public string name { get; set; }
+            public string alt_name { get; set; }
+            public string description { get; set; }
+            public object trailer { get; set; }
+            public string status { get; set; }
+            public string type { get; set; }
+            public int episode_count { get; set; }
+            public string parental_rating { get; set; }
+            public string release_date { get; set; }
+            public string end_date { get; set; }
+            public string broadcast { get; set; }
+            public string premiered { get; set; }
+            public string episode_at { get; set; }
+            public string created_at { get; set; }
+            public string updated_at { get; set; }
+            public int episodes_count { get; set; }
+            public string slug { get; set; }
+            public bool is_collected { get; set; }
+            public object collection_status { get; set; }
+            public List<AnimeFeverSearchPoster> posters { get; set; }
+            public object backgrounds { get; set; }
+            public AnimeFeverSearchPoster2 poster { get; set; }
+            public AnimeFeverSearchLogo logo { get; set; }
+            public object background { get; set; }
+        }
+
+        public class AnimeFeverSearchInfo
+        {
+            public int current_page { get; set; }
+            public List<AnimeFeverSearchDatum> data { get; set; }
+            public string first_page_url { get; set; }
+            public int from { get; set; }
+            public int last_page { get; set; }
+            public string last_page_url { get; set; }
+            public object next_page_url { get; set; }
+            public string path { get; set; }
+            public int per_page { get; set; }
+            public object prev_page_url { get; set; }
+            public int to { get; set; }
+            public int total { get; set; }
+        }
+         
+        public class AnimeFeverEpisodeImage
+        {
+            public int id { get; set; }
+            public string disk_name { get; set; }
+            public int file_size { get; set; }
+            public string content_type { get; set; }
+            public object title { get; set; }
+            public object description { get; set; }
+            public string field { get; set; }
+            public int sort_order { get; set; }
+            public string created_at { get; set; }
+            public string updated_at { get; set; }
+            public string path { get; set; }
+            public string extension { get; set; }
+        }
+
+        public class AnimeFeverEpisodeVideoMeta
+        {
+            public List<string> audio_languages { get; set; }
+            public string status { get; set; }
+            public long download_size { get; set; }
+        }
+
+        public class AnimeFeverEpisodeDatum
+        {
+            public int id { get; set; }
+            public string title { get; set; }
+            public string slug { get; set; }
+            public string number { get; set; }
+            public int duration { get; set; }
+            public AnimeFeverEpisodeImage image { get; set; }
+            public AnimeFeverEpisodeVideoMeta video_meta { get; set; }
+            public int is_filler { get; set; }
+            public int is_recap { get; set; }
+            public bool watched { get; set; }
+            public object progress { get; set; }
+        }
+
+        public class AnimeFeverEpisodeLinks
+        {
+            public string first { get; set; }
+            public string last { get; set; }
+            public object prev { get; set; }
+            public object next { get; set; }
+        }
+
+        public class AnimeFeverEpisodeMeta
+        {
+            public int current_page { get; set; }
+            public int from { get; set; }
+            public int last_page { get; set; }
+            public string path { get; set; }
+            public int per_page { get; set; }
+            public int to { get; set; }
+            public int total { get; set; }
+        }
+
+        public class AnimeFeverEpisodeInfo
+        {
+            public List<AnimeFeverEpisodeDatum> data { get; set; }
+            public AnimeFeverEpisodeLinks links { get; set; }
+            public AnimeFeverEpisodeMeta meta { get; set; }
+        }
+
+        static readonly string[] headerValue = new string[] { "animefever", "cloudflare" };
+        static readonly string[] headerName = new string[] { "AF-Access-API", "server-provider" };
+        public AnimeFeverSearchInfo GetSearchResults(string search, bool isMovie)
+        {
+            /*   webRequest.Headers.Add("AF-Access-API", "animefever");
+            webRequest.Headers.Add("server-provider", "cloudflare");*/
+            string qry = $"https://www.animefever.tv/api/anime/shows?search={search}&sortBy=name+asc&type[]={(isMovie ? "Movie" : "TV")}&hasVideos=true&hasMultiAudio=false&page=1";
+            string d = core.DownloadString(qry, referer: "https://www.animefever.tv/series",
+                headerName: headerName, headerValue: headerValue);
+            if (d == "") {
+                return null;
+            }
+            return JsonConvert.DeserializeObject<AnimeFeverSearchInfo>(d);
+        }
+
+        public AnimeFeverEpisodeInfo GetAnimeFeverEpisodeInfo(int id, string slug)
+        {
+            string qry = $"https://www.animefever.tv/api/anime/details/episodes?id={id}-{slug}";
+            string d = core.DownloadString(qry, referer: "https://www.animefever.tv/series",
+                headerName: headerName, headerValue: headerValue);
+            if (d == "") {
+                return null;
+            }
+            return JsonConvert.DeserializeObject<AnimeFeverEpisodeInfo>(d);
+        }
+
+        public string GetAnimeFeverEpisodeStream(int id)
+        {
+            string qry = $"  https://www.animefever.tv/video/{id}/stream.m3u8";
+            string d = core.DownloadString(qry, referer: "https://www.animefever.tv/series",
+                headerName: headerName, headerValue: headerValue);
+
+            return d;
+        }
+
+        CloudStreamCore core;
+        public AnimeFeverHelper(CloudStreamCore _core)
+        {
+            core = _core;
+        }
+
+    }
+
+}
---
+++ CloudStream2Console/Core/AnimeProviders/NineAnimeBloatFreeProvider.cs
@@ -0,0 +1,177 @@
+﻿using HtmlAgilityPack.CssSelectors.NetCore;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using static CloudStreamForms.Core.BlotFreeProvider;
+using static CloudStreamForms.Core.CloudStreamCore;
+
+namespace CloudStreamForms.Core.AnimeProviders
+{
+    public class NineAnimeBloatFreeProvider : BloatFreeBaseAnimeProvider
+    {
+        public NineAnimeBloatFreeProvider(CloudStreamCore _core) : base(_core) { }
+
+        public override string Name => "9Anime";
+
+        [System.Serializable]
+        public struct NineAnimeEpisodeData
+        {
+            public int ep;
+            public string href;
+            public string id;
+        }
+
+        public override void LoadLink(string episodeLink, int episode, int normalEpisode, TempThread tempThred, object extraData,bool isDub)
+        {
+            string request = episodeLink.Replace("https://9anime.to/watch/", "") + "/"; // /xrrj358";
+            string url = "https://9anime.to/watch/" + request;
+            string d = DownloadString(url);
+            string key = core.GetMcloudKey(url);
+
+            string dataTs = FindHTML(d, "data-ts=\"", "\"");
+            string _id = FindHTML(request, ".", "/");
+            // string _endId = FindHTML(request + "|||", "/", "|||");
+            string _under = rng.Next(100, 999).ToString();
+            // print(_id + "|" + _endId);
+
+            string requestServer = $"https://9anime.to/ajax/film/servers?id={_id}&ts={dataTs}&_={_under}"; // &episode={_endId}
+            string serverResponse = DownloadString(requestServer).Replace("\\", "").Replace("  ", "");
+            //   print(serverResponse);
+            string real = FindHTML(serverResponse, "{\"html\":\"", "\"}");
+            // real = real.Substring(0, real.Length - 2);
+            print(real);
+
+
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(real);
+            var data = doc.QuerySelectorAll("div.server");
+
+            List<NineAnimeEpisodeData> Streamtape = new List<NineAnimeEpisodeData>();
+            List<NineAnimeEpisodeData> Mp4upload = new List<NineAnimeEpisodeData>();
+            List<NineAnimeEpisodeData> MyCloud = new List<NineAnimeEpisodeData>();
+            const string myCloudId = "28";
+            const string streamtapeId = "40";
+            const string mp4uploadId = "35";
+
+
+            foreach (var subData in data) {
+
+                string name = subData.GetAttributeValue("ndata-id", "");
+
+                foreach (var selectors in subData.QuerySelectorAll("ul > li > a")) {
+                    string subId = selectors.GetAttributeValue("ndata-id", "");
+                    string subEp = selectors.GetAttributeValue("ndata-base", "");
+                    string href = selectors.GetAttributeValue("nnhref", "");
+                    int realEp = int.Parse(selectors.InnerText);
+                    var storeData = new NineAnimeEpisodeData() {
+                        ep = realEp,
+                        href = href,
+                        id = subId,
+                    };
+
+                    if (name == myCloudId) {
+                        MyCloud.Add(storeData);
+                    }
+                    else if (name == streamtapeId) {
+                        Streamtape.Add(storeData);
+                    }
+                    else if (name == mp4uploadId) {
+                        Mp4upload.Add(storeData);
+                    }
+
+                }
+            }
+
+            try {
+                string target = core.GetTarget(Mp4upload[normalEpisode].id, key, dataTs, "https://9anime.to", url);
+                AddMp4(FindHTML(target, "embed-", "."), normalEpisode, tempThred);
+            }
+            catch (Exception) {
+
+            }
+
+            try {
+                core.AddStreamTape(Streamtape[normalEpisode].id, key, dataTs, "https://9anime.to", normalEpisode, url);
+            }
+            catch (Exception) {
+
+            }
+
+            try {
+                core.AddMCloud(MyCloud[normalEpisode].id, key, dataTs, "https://9anime.to", normalEpisode, url); 
+            }
+            catch (Exception) {
+
+            }
+
+        }
+
+        public override object StoreData(string year, TempThread tempThred, MALData malData)
+        {
+            return Search(malData.engName);
+        }
+
+        public override NonBloatSeasonData GetSeasonData(MALSeason ms, TempThread tempThread, string year, object storedData)
+        {
+            List<NineAnimeDataSearch> data = (List<NineAnimeDataSearch>)storedData;
+            print("START:::: " + data.FString());
+            NonBloatSeasonData setData = new NonBloatSeasonData() { dubEpisodes = new List<string>(), subEpisodes = new List<string>() };
+            foreach (var subData in data) {
+                if (subData.names.Split(' ').Contains(ms.japName)) {
+                    bool isDub = subData.isDub;
+                    if (isDub && !setData.dubExists) {
+                        for (int i = 1; i <= subData.maxEp; i++) {
+                            setData.dubEpisodes.Add(subData.href);
+                        }
+                    }
+                    else if (!setData.subExists) {
+                        for (int i = 1; i <= subData.maxEp; i++) {
+                            setData.subEpisodes.Add(subData.href);
+                        }
+                    }
+                }
+            }
+            return setData;
+        }
+
+        [Serializable]
+        public struct NineAnimeDataSearch
+        {
+            public string title;
+            public string names;
+            public bool isDub;
+            public string href;
+            public int maxEp;
+        }
+
+        List<NineAnimeDataSearch> Search(string search)
+        {
+            List<NineAnimeDataSearch> searchData = new List<NineAnimeDataSearch>();
+
+            string url = "https://9anime.to/search?keyword=" + search;
+            string d = DownloadString(url);
+            print(d);
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(d);
+            var data = doc.QuerySelector("div.film-list");
+            var items = data.QuerySelectorAll("div.item > div.inner > a");
+            foreach (var item in items) {
+                string href = item.GetAttributeValue("href", "");
+                string dataTip = item.GetAttributeValue("data-tip", "");
+
+                if (dataTip != "") {
+                    string _d = DownloadString("https://9anime.to" + dataTip, referer: url);
+                    int.TryParse(FindHTML(_d, "Episode ", "/"), out int maxEp);
+                    if (maxEp != 0) { // IF NOT MOVIE 
+                        string otherNames = FindHTML(_d, "<label>Other names:</label>\n            <span>", "<").Replace("  ", "").Replace("\n", "").Replace(";", "");
+                        string title = FindHTML(_d, "data-jtitle=\"", "\"");
+                        print(maxEp + "|" + title + "|" + otherNames.FString());
+                        bool isDub = title.Contains("(Dub)");
+                        searchData.Add(new NineAnimeDataSearch() { isDub = isDub, maxEp = maxEp, href = href, names = otherNames, title = title.Replace(" (Dub)", "").Replace("  ", "") });
+                    }
+                }
+            }
+            return searchData;
+        }
+    }
+}
